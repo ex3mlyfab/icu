2e8560d7775246702cf67f165fd310e3d1cefbe8
Abdulkadir Olatunji <tunjikadri@gmail.com>
2024-10-18 23:59:52 +0100

    corrected migrotion history

--- database/migrations/2024_10_13_165327_create_bed_occupation_histories_table.php
+++ database/migrations/2024_10_13_165327_create_bed_occupation_histories_table.php
@@ -2,6 +2,7 @@
 
 use App\Models\BedModel;
 use App\Models\Patient;
+use App\Models\PatientCare;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
         Schema::create('bed_occupation_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(BedModel::class, 'bed_model_id')->constrained();
-            $table->foreignIdFor(Patient::class, 'patient_id')->constrained();
+            $table->foreignIdFor(PatientCare::class, 'patient_care_id')->constrained();
             $table->date('start_date');
             $table->date('end_date')->nullable();
             $table->boolean('is_occupied')->default(0);
